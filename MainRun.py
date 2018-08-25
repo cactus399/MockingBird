34a97b8054c917872a542af981c9ac0ab7013589
Sung-Wook J Bang <cactus399@gmail.com>
2018-08-25 03:51:51 -0400

    MimicBrowsing.PlatformBrowser.ctor2 WORKS!
    _subject_id is default 103 (i picked any number), but can be switched.
    This will grab every single event for that particular patient across all tables.

--- MainRun.py
+++ MainRun.py
@@ -5,12 +5,81 @@ import datetime
 import pandas
 import numpy
 
-msp = MimicServer.MimicServerPlatform.ctor0("postgres", "public")
-msp.connect()
-thecurse = msp.connection.cursor("curse")
-thestr = "SELECT * FROM patients;"
-thecurse.execute(thestr)
-avar = thecurse.fetchall()
+pb = MimicBrowsing.PlatformBrowser.ctor2("postgres", "public", "500")
+whole = pb.readall()
+for eachthing in whole:
+    print(eachthing)
+# while True:
+#     athing = pb.readone()
+#     print(athing)
+#     if athing == None:
+#         break
 
-for athing in avar:
-    print(athing)
+# print(pb.sqlcommandstring)
+## whole = pb.readall()
+## print(len(whole))
+# anum = len(whole)
+#print(anum)
+# for eachline in whole:
+#     print(len(eachline))
+
+# sample = ["lol", "lol2"]
+# alist = ["1"]
+# twolist = alist * len(sample)
+# print(twolist)
+
+
+    # for eachthing in eachline:
+    #     print(eachthing)
+
+# athing = pb.readone()
+
+# print(athing)
+# athing = pb.readone()
+# print(athing)
+
+# msp = MimicServer.MimicServerPlatform.ctor0("postgres", "public")
+# msp.connect()
+# thecurse = msp.connection.cursor("curse")
+# thestr = "SELECT * FROM patients;"
+# thecurse.execute(thestr)
+# avar = thecurse.fetchone()
+# print(thecurse.description[0][0])
+#
+# for athing in avar:
+#     print(athing)
+
+# # class A:
+# #     def __init__(self):
+# #         self._val1 = 1
+# #         self._val2 = 2
+# #
+# # class B:
+# #     def __init__(self):
+# #         self._val3 = 3
+# #         self._val4 = 4
+# #
+# # class C(A, B):
+# #     def __init__(self):
+# #         super().__init__()
+# #
+# #     @classmethod
+# #     def ctor0(cls):
+# #         return cls()
+# #
+# #     @classmethod
+# #     def ctor1(cls, _a, _b):
+# #         thisguy = cls()
+# #         thisguy.__dict__.update(_a.__dict__)
+# #         thisguy.__dict__.update(_b.__dict__)
+# #         #thisguy.__dict__.update(_a)
+# #         #thisguy.__dict__.update(_b)
+# #         return thisguy
+# #
+# #
+# # a = A()
+# # b = B()
+# # c = C.ctor1(a, b)
+# # print(c._val1)
+# # print(c._val4)
+# -
--- MimicBrowsing.py
+++ MimicBrowsing.py
@@ -159,6 +159,12 @@ class ConditionCollection:
     def __repr__(self):
         return self.__str__()
 
+    def __len__(self):
+        return len(self._unitsbundles)
+
+    def __iter__(self):
+        return iter(self._unitsbundles)
+
     @property
     def filters(self):
         return self._unitsbundles
@@ -169,7 +175,10 @@ class ConditionCollection:
 
     @property
     def filtercount(self):
-        return len(self._unitsbundles)
+        if self._unitsbundles != None:
+            return len(self._unitsbundles)
+        else:
+            return 0
 
     @property
     def logicalcount(self):
@@ -317,9 +326,55 @@ class MimicCursor:
     #     self._tableindex = _tableindex
     #     self._sqlcmd = _sqlcmd
 
-    def __init__(self, _tablesearchspace=[], _tableindex=-1):
+    def __init__(self, _tablesearchspace=[], _tableindex=-1, _filterset=[], _columnselection=[]):
+        #_filterset=ConditionCollection(), _columnselection=SqlColumnCollection()):
         self._tablesearchspace = _tablesearchspace
         self._tableindex = _tableindex
+        self._columnselection = _columnselection
+        self._filterset = _filterset
+
+    @classmethod
+    def ctor0(cls):
+        return cls()
+
+    @classmethod
+    def ctor1(cls, _tablesearchspace=["patients","caregivers"], _tableindex=0, _columnselection=["*"]):
+        #, "labevents", "cptevents", "datetimeevents"], _tableindex=0, _columnselection=["*"]):
+
+        _columnselection = ["*"]*len(_tablesearchspace)
+        thisguy = cls(_tablesearchspace=_tablesearchspace, _tableindex=_tableindex, _columnselection=_columnselection)
+        #thisguy._columnselection = thisguy._columnselection * len(_tablesearchspace)
+        return thisguy
+
+    @classmethod
+    def ctor2(cls, _tablesearchspace=["chartevents", "labevents", "cptevents", "datetimeevents"], _tableindex=0, _columnselection=["*"], _subject_id="103"):
+        cu = ConditionUnit("subject_id,=,"+_subject_id)
+        cb = ConditionBundle(_conditionunits=[cu])
+        cc = ConditionCollection(_unitsbundles=[cb])
+        _filters = [cc] * len(_tablesearchspace)
+        _columnselection = ["*"] * len(_tablesearchspace)
+        thisguy = cls(_tablesearchspace=_tablesearchspace, _tableindex=_tableindex, _columnselection=_columnselection, _filterset=_filters)
+        # thisguy._columnselection = thisguy._columnselection * len(_tablesearchspace)
+        return thisguy
+
+    @property
+    def ___hide___columns(self):
+        return self._columnselection[self.focusedtableindex]
+
+    @property
+    def ___hide___filters(self):
+        return self._filterset[self.focusedtableindex]
+        # if self._filterset != None:
+        #     if self.focusedtableindex < len(self._filterset):
+        #         return self._filterset[self.focusedtableindex]
+        # else:
+        #     return ""
+
+    @property
+    def sqlcommandstring(self):
+        aguy = SqlCommand(_columns=self.___hide___columns, _filtercoll=ConditionCollection(self.___hide___filters), _tablename=self.focusedtablename)
+        return str(aguy)
+
 
     @property
     def tablesearchspace(self):
@@ -339,10 +394,11 @@ class MimicCursor:
 
     @focusedtableindex.setter
     def focusedtableindex(self, _tableindexvalue):
-        if _tableindexvalue < self.tablesearchspaceLength:
-            self._tableindex = _tableindexvalue
-        else:
-            self._tableindex = self.tablesearchspaceLength - 1
+        self._tableindex = _tableindexvalue
+        # if _tableindexvalue < self.tablesearchspaceLength:
+        #     self._tableindex = _tableindexvalue
+        # else:
+        #     self._tableindex = self.tablesearchspaceLength - 1
 
     @property
     def focusedtablename(self):
@@ -362,10 +418,121 @@ class MimicCursor:
             else:
                 counter += 1
 
+
 class PlatformBrowser(MimicServer.MimicServerPlatform, MimicCursor):
     def __init__(self):
         super().__init__()
+        self._psycocursor = None
 
+    @classmethod
+    def ctor0(cls):
+        return cls()
+
+    @classmethod
+    def ctor1(cls, _db, _sch):
+        thisguy = PlatformBrowser.ctor0()
+        thisguy.__dict__.update(MimicServer.MimicServerPlatform.ctor0(_db=_db, _sch=_sch).__dict__)
+        thisguy.__dict__.update(MimicCursor.ctor1().__dict__)
+        thisguy.connect()
+        thisguy._psycocursor = thisguy.connection.cursor()#(name="psycocursor")
+        thisguy._psycocursor.execute(thisguy.sqlcommandstring)
+        return thisguy
+
+    @classmethod
+    def ctor2(cls, _db, _sch, _subject_id="103"):
+        thisguy = PlatformBrowser.ctor0()
+        thisguy.__dict__.update(MimicServer.MimicServerPlatform.ctor0(_db=_db, _sch=_sch).__dict__)
+        thisguy.__dict__.update(MimicCursor.ctor2(_subject_id=_subject_id).__dict__)
+        thisguy.connect()
+        thisguy._psycocursor = thisguy.connection.cursor()#(name="psycocursor")
+        thisguy._psycocursor.execute(thisguy.sqlcommandstring)
+        return thisguy
+
+    @property
+    def cursor(self):
+        return self._psycocursor
+
+    @cursor.setter
+    def cursor(self, _cursorvalue):
+        self._psycocursor = _cursorvalue
+
+    def readone(self):
+        # check if cursor is closed or not
+        if self.cursor.closed == True: # means it is closed
+            self.cursor = self.connection.cursor()
+            astr = self.sqlcommandstring
+            self.cursor.execute(astr)
+        athingthing = self.cursor.fetchone()
+        if athingthing == None:
+            self.cursor.close()
+            self.advance()
+            if self.focusedtableindex >= self.tablesearchspaceLength:
+                return None
+            else:
+                return self.readone()
+                #self.cursor.execute(self.sqlcommandstring)
+        return athingthing
+        #return self.cursor.fetchone()
+
+    def readall(self):
+        alldat = []
+        while self.canadvance() == True:
+            if self.cursor.closed == True:
+                self.cursor = self.connection.cursor()
+                self.cursor.execute(self.sqlcommandstring)
+            onetable = self.cursor.fetchall()
+            alldat.extend(onetable)
+            self.cursor.close()
+            self.advance()
+        return alldat
+
+        # alldat = []
+        # while self.canadvance() == True:
+        #     if self.cursor.closed == True:
+        # #        print("check1")
+        #         self.advance()
+        #         self.cursor.execute(self.sqlcommandstring)
+        #     athing = self.cursor.fetchall()
+        # #    print("check2")
+        #     if athing != None:
+        #         alldat.append(athing)
+        #         self.cursor.close()
+        # #        print("check3")
+        #     self.advance()
+        # #     print("check4")
+        # #     print(self.focusedtableindex)
+        # # print(self.canadvance())
+        # return alldat
+
+        # alldat = []
+        # while True:
+        #     athing = self.readone()
+        #     if athing == None:
+        #             return alldat
+        #     else:
+        #         alldat.append(athing)
+        # return None
+
+    def advance(self):
+        self.focusedtableindex += 1
+
+    def canadvance(self):
+        if self.focusedtableindex < self.tablesearchspaceLength:
+            return True
+        else:
+            return False
+
+    def close(self):
+        self._psycocursor.close()
+        self.connection.close()
+
+
+    # @classmethod
+    # def ctor1(cls, _targettables, _mc):
+    #     thisguy = cls()
+    #     thisguy.tablesearchspace = _targettables
+    #     thisguy.focusedtableindex = 0
+    #     return thisguy
 
 
 
